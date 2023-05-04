5e3eaa4798d65716e35463852691627d7727fa20
NhatTran95 <tranducanhnhat@gmail.com>
2023-05-04 21:00:18 +0700

    up

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+class ListProducts{
+    constructor(img, name, desc, price) {
+        this.img = img;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+    getImg(){
+        return this.img
+    }
+    getName() {
+        return this.name
+    }
+    getDesc(){
+        return this.desc
+    }
+    getPrice(){
+        return this.price
+    }
+}
+let products = [
+    new ListProducts("assets/product_1.png", "Orient Sports Diver's Watch", "Nổi bật với Dây đeo tông màu bạc, Vỏ tông màu bạc, Tinh thể sapphire chống trầy xước", "3.969.996"),
+    new ListProducts("assets/product_2.png", "Orient Bambino Watch", "Đồng hồ đeo tay bằng da và thép không gỉ tự động Orient '2nd Gen Bambino Version III' của Nhật Bản", "2.899.999"),
+    new ListProducts("assets/product_3.png", "Casio MDV106 Watch", "Đồng hồ Casio nam MDV106-1AV 200M Duro Analog, màu đen, khả năng chống nước 200M", "3.799.999"),
+    new ListProducts("assets/product_4.png", "Timex X Todd Snyder Mk1 'Black + White' 40mm Watch", "Nổi bật với hai màu đen và trắng, chất liệu thép không gỉ", "4.686.686"),
+    new ListProducts("assets/product_5.png", "Audemars Piguet Royal Oak Offshore Watch", "Kích cỡ 42mm, độ dày 15.3mm, khả năng chống nước 100 M", "4.999.966"),
+    new ListProducts("assets/product_6.png", "Hublot Big Bang Sang Bleu II Watch", "Táo bạo, mạnh mẽ, độc đáo và kỹ thuật, chất liệu gốm sứ công nghệ cao siêu bên chống trầy xước", "5.899.999"),
+    new ListProducts("assets/product_7.png", "Shinola Sea Creatures Watch", "Chế tạo từ nhựa có nguồn gốc từ đại dương với lõi bằng thép không gỉ, nhập khẩu từ Thụy Sỹ", "3.769.796"),
+    new ListProducts("assets/product_8.png", "Breitling Exospace B55 SuperQuartz Titanium Watch", "Vỏ titan đen khung hai chiều,thấu kính tinh thể sapphire", "4.969.696")
+]
+function displayProducts() {
+    let content = ``;
+    for (let i = 0; i < products.length; i++) {
+        content += `<div class="item">
+        <img src=${products[i].getImg()} alt="">
+        <div class="stars">
+            <span><img src="assets/star.png" alt=""></span>
+            <span><img src="assets/star.png" alt=""></span>
+            <span><img src="assets/star.png" alt=""></span>
+            <span><img src="assets/star.png" alt=""></span>
+            <span><img src="assets/star.png" alt=""></span>
+        </div>
+        <div class="name">${products[i].getName()}</div>
+        <div class="desc">${products[i].getDesc()}</div>
+        <div class="price"><span>${products[i].getPrice()}</span> vnđ</div>
+        <button>Thêm vào giỏ hàng</button>
+        </div>`
+    }
+    document.getElementById("list-products").innerHTML = content;
+}
+displayProducts();
 const btn = document.querySelectorAll(".item button")
 btn.forEach(function (button, index) {
     button.addEventListener("click", function (event) {
